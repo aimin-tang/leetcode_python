05d332a85e9317098fb9953d4635ed2570b754fa
Aimin Tang <aimin.tang@gmail.com>
2020-10-22 10:19:31 -0500

    q5 slow

--- src/longest_palindrome_5.py
+++ src/longest_palindrome_5.py
@@ -1,1 +1,28 @@
 class Solution:
+    def is_palindrome(self, s):
+        return s == s[::-1]
+
+    def longest_from(self, s: str, pos: int) -> str:
+        # empty should be handled before here
+        longest = s[pos]
+        sub_string = s[pos:]
+        # can improve a lot on speed
+        for i in range(len(sub_string), 1, -1):
+            if self.is_palindrome(sub_string[:i]):
+                if len(sub_string[:i]) > len(longest):
+                    longest = sub_string[:i]
+                break
+
+        return longest
+
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) == 0:
+            return ""
+
+        result = []
+        for i in range(len(s)):
+            result.append(self.longest_from(s, i))
+            result.sort(key=lambda x: len(x), reverse=True)
+
+        return result[0]
+
--- tests/test_longest_palindrome_5.py
+++ tests/test_longest_palindrome_5.py
@@ -1,5 +1,39 @@
 from longest_palindrome_5 import Solution
 
 
+def test_is_palindrom():
+    r = Solution().is_palindrome('')
+    assert r == True
+    r = Solution().is_palindrome('aba')
+    assert r == True
+    r = Solution().is_palindrome('abc')
+    assert r == False
+
+
+def test_longest_from():
+    r = Solution().longest_from('babad', 0)
+    assert r == 'bab'
+    r = Solution().longest_from('babad', 1)
+    assert r == 'aba'
+    r = Solution().longest_from('babad', 2)
+    assert r == 'b'
+
 
 def test_with_ex1():
+    r = Solution().longestPalindrome('babad')
+    assert r in ['bab', 'aba']
+
+
+def test_with_ex2():
+    r = Solution().longestPalindrome('cbbd')
+    assert r == 'bb'
+
+
+def test_with_ex3():
+    r = Solution().longestPalindrome('a')
+    assert r == 'a'
+
+
+def test_with_ex4():
+    r = Solution().longestPalindrome('ac')
+    assert r in ['a', 'c']
