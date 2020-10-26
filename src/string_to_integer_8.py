4e53db312d23ad554355f12fb9fa8c052fa80257
Aimin Tang <aimin.tang@gmail.com>
2020-10-25 21:06:24 -0500

    q8 done

--- src/string_to_integer_8.py
+++ src/string_to_integer_8.py
@@ -1,1 +1,64 @@
 class Solution:
+    def is_digit(self, c: str) -> bool:
+        digits = [str(d) for d in range(10)]
+        return c in digits
+
+    def handle_leading_sign(self, s: str) -> tuple:
+        sign = 1
+        if len(s) > 0:
+            if s[0] == '-':
+                sign = -1
+                s = s[1:]
+            elif s[0] == '+':
+                sign = 1
+                s = s[1:]
+        else:
+            return 1, ""
+
+        return sign, s
+
+    def handle_trailing_words(self, s:str) -> str:
+        for idx, c in enumerate(s):
+            if self.is_digit(c) or c == '.':
+                continue
+            else:
+                return s[:idx]
+
+        return s
+
+    def handle_dot(self, s: str) -> str:
+        s = s.split('.')
+        if len(s) == 0:
+            return ""
+        else:
+            return s[0]
+
+    def handle_out_of_range(self, i: int) -> int:
+        if i > 2 ** 31 - 1:
+            return 2 ** 31 - 1
+        elif i < 2 ** 31 * (-1):
+            return 2 ** 31 * (-1)
+        else:
+            return i
+
+    def myAtoi(self, s: str) -> int:
+        # handle leading spaces
+        s = s.strip()
+
+        # handle leading "+" or "-"
+        sign, s = self.handle_leading_sign(s)
+
+        # handle trailing words
+        s = self.handle_trailing_words(s)
+
+        # handle "."
+        s = self.handle_dot(s)
+
+        # convert it
+        result = 0
+        for i in range(len(s)):
+            result = result * 10 + int(s[i])
+        result *= sign
+
+        # handle out of range
+        return self.handle_out_of_range(result)
--- tests/test_string_to_integer_8.py
+++ tests/test_string_to_integer_8.py
@@ -2,4 +2,29 @@ from string_to_integer_8 import Solution
 
 
 def test_with_ex1():
+    r = Solution().myAtoi("42")
+    assert r == 42
 
+def test_with_ex2():
+    r = Solution().myAtoi("   -42")
+    assert r == -42
+
+def test_with_ex3():
+    r = Solution().myAtoi("4193 with words")
+    assert r == 4193
+
+def test_with_ex4():
+    r = Solution().myAtoi("words and 42")
+    assert r == 0
+
+def test_with_ex5():
+    r = Solution().myAtoi("   -91283472332")
+    assert r == -2147483648
+
+def test_with_ex6():
+    r = Solution().myAtoi("3.14")
+    assert r == 3
+
+def test_with_ex7():
+    r = Solution().myAtoi("  -0012a42")
+    assert r == -12
