05331dc04b5f66c77c10f37c0acddb228ae37c81
Aimin Tang <aimin.tang@gmail.com>
2020-10-29 09:26:37 -0500

    q17 solved

--- src/letter_combinations_of_a_phone_number_17.py
+++ src/letter_combinations_of_a_phone_number_17.py
@@ -1,1 +1,33 @@
+from typing import List
+from itertools import chain
+
+
 class Solution:
+    def digit_to_letters(self, digit: str) -> List[str]:
+        d2l = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+        return d2l[digit]
+
+    def letterCombinations(self, digits: str) -> List[str]:
+        result = []
+
+        for digit in digits:
+            letters = self.digit_to_letters(digit)
+            tmp_list = []
+            if result:    # not empty
+                for letter in letters:
+                    tmp_list.append([w+letter for w in result])
+            else:         # empty result
+                tmp_list = letters
+
+            result = list(chain.from_iterable(tmp_list))
+
+        return result
--- tests/test_letter_combinations_of_a_phone_number_17.py
+++ tests/test_letter_combinations_of_a_phone_number_17.py
@@ -1,5 +1,21 @@
 from letter_combinations_of_a_phone_number_17 import Solution
 
 
-def test_with_ex1():
+def test_digit_to_letters():
+    r = Solution().digit_to_letters('3')
+    assert r == ['d', 'e', 'f']
 
+
+def test_letterCombinations_ex1():
+    r = Solution().letterCombinations('23')
+    assert set(r) == set(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
+
+
+def test_letterCombinations_ex2():
+    r = Solution().letterCombinations('')
+    assert set(r) == set([])
+
+
+def test_letterCombinations_ex3():
+    r = Solution().letterCombinations('2')
+    assert set(r) == set(["a", "b", "c"])
