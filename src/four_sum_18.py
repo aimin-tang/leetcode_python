93e71bb50e946dde325ab61ea90ece736407e647
Aimin Tang <aimin.tang@gmail.com>
2020-11-03 10:23:21 -0600

    q18 solved

--- src/four_sum_18.py
+++ src/four_sum_18.py
@@ -1,13 +1,42 @@
 from typing import List
-from itertools import combinations
 
 
 class Solution:
-    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+    def get_last_two(self, nums: List[int], target: int, pos1: int, pos2: int) -> List[List[int]]:
+        """
+        If pos1 and pos2 are known for the first two numbers, find all solutions
+        """
+        l = pos2 + 1
+        r = len(nums) - 1
         result = []
-        for c in combinations(nums, 4):
-            c = sorted(list(c))
-            if sum(c) == target and c not in result:
-                result.append(c)
 
-        return sorted(result)
+        while l < r:
+            if nums[pos1] + nums[pos2] + nums[l] + nums[r] < target:
+                l += 1
+            elif nums[pos1] + nums[pos2] + nums[l] + nums[r] > target:
+                r -= 1
+            else:
+                one_answer = [nums[pos1], nums[pos2], nums[l], nums[r]]
+                one_answer.sort()
+                result.append(one_answer)
+                l += 1
+                r -= 1
+
+        return result
+
+
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        if len(nums) < 4:
+            return []
+
+        result = []
+        for pos1 in range(len(nums) - 3):
+            for pos2 in range(pos1+1, len(nums) - 2):
+                result.extend(self.get_last_two(nums, target, pos1, pos2))
+
+        result2 = []
+        for r in result:
+            if r not in result2:
+                result2.append(r)
+        return result2
--- tests/test_four_sum_18.py
+++ tests/test_four_sum_18.py
@@ -1,5 +1,12 @@
 from four_sum_18 import Solution
 
+def test_get_last_two():
+    nums = [-2, -1, 0, 0, 1, 2]
+    target = 0
+    r = Solution().get_last_two(nums, target, 0, 1)
+    assert r == [[-2, -1, 1, 2]]
+    r = Solution().get_last_two(nums, target, 0, 2)
+    assert r == [[-2, 0, 0, 2]]
 
 def test_with_ex1():
     nums = [1, 0, -1, 0, -2, 2]
