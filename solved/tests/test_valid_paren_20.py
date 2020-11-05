fe6b56694f6d8fbd9367f9150a39b38612854795
Aimin Tang <aimin.tang@gmail.com>
2020-11-04 20:50:46 -0600

    q20 solved

---
+++ solved/tests/test_valid_paren_20.py
@@ -0,0 +1,5 @@
+from valid_paren_20 import Solution
+
+
+def test_with_ex1():
+
--- src/valid_paren_20.py
+++ src/valid_paren_20.py
@@ -1,1 +1,43 @@
 class Solution:
+    def safe_to_remove(self, s: str, pos: int) -> bool:
+        """
+        if s[pos] and s[pos+1] is matching parens, return true.
+        """
+        if pos == len(s) - 1:
+            return False
+
+        if s[pos] == '(' and s[pos+1] == ')' or \
+            s[pos] == '[' and s[pos + 1] == ']' or \
+            s[pos] == '{' and s[pos + 1] == '}':
+            return True
+
+        return False
+
+    def remove_two_chars(self, s: str, pos: int) -> str:
+        """
+        remove s[pos] and s[pos+1] from string
+        """
+        if pos == 0:
+            return s[2:]
+        else:
+            return s[:pos] + s[pos+2:]
+
+    def isValid(self, s: str) -> bool:
+        s2 = ''.join([l for l in s])  # s isn't changed by this function
+        made_progress = True
+
+        while made_progress:
+            made_progress = False
+            for pos in range(len(s2)):
+                if self.safe_to_remove(s2, pos):
+                    s2 = self.remove_two_chars(s2, pos)
+                    made_progress = True
+                    break
+
+        if len(s2) == 0:
+            return True
+        else:
+            return False
+
+
+
