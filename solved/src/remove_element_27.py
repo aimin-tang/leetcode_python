0255e3d7c6398d698923a0d7158838e188325db3
Aimin Tang <aimin.tang@gmail.com>
2020-11-09 18:41:33 -0600

    q27 solved

---
+++ solved/src/remove_element_27.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        for i in reversed(range(len(nums))):
+            if nums[i] == val:
+                del nums[i]
+
+        return len(nums)
---
+++ tests/test_strstr_28.py
@@ -0,0 +1,5 @@
+from strstr_28 import Solution
+
+
+def test_with_ex1():
+
