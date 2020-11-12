b8d2542538ddc9e3bf4e611d4d3d9ccec7dc5e59
Aimin Tang <aimin.tang@gmail.com>
2020-11-11 20:16:12 -0600

    q29 not fast enough

--- src/divide_two_ints_29.py
+++ src/divide_two_ints_29.py
@@ -1,1 +1,23 @@
 class Solution:
+    def divide(self, dividend: int, divisor: int) -> int:
+        # get the sign
+        if (dividend > 0 and divisor > 0) or (dividend < 0 and divisor < 0):
+            sign = 1
+        elif (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0):
+            sign = -1
+        elif divisor == 0:
+            raise ValueError("divisor should not be 0")
+        else:   # dividend is 0
+            return 0
+
+        dividend = abs(dividend)
+        divisor = abs(divisor)
+
+        result = 0
+        curr_divident = dividend
+
+        while curr_divident >= divisor:
+            curr_divident -= divisor
+            result += 1
+
+        return result * sign
--- tests/test_divide_two_ints_29.py
+++ tests/test_divide_two_ints_29.py
@@ -2,4 +2,11 @@ from divide_two_ints_29 import Solution
 
 
 def test_with_ex1():
-
+    r = Solution().divide(10, 3)
+    assert r == 3
+    r = Solution().divide(7, -3)
+    assert r == -2
+    r = Solution().divide(0, 1)
+    assert r == 0
+    r = Solution().divide(1, 1)
+    assert r == 1
