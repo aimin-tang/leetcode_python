ff2e8575d54b8900bee3286522e61d3b32225ce9
Aimin Tang <aimin.tang@gmail.com>
2020-11-15 15:27:02 -0600

    redo q1

--- src/two_sum_1.py
+++ src/two_sum_1.py
@@ -1,17 +1,21 @@
 from typing import List
+from collections import defaultdict
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d = defaultdict(list)
+
+        for idx, num in enumerate(nums):
+            d[num].append(idx)
+
         for idx, num in enumerate(nums):
             diff = target - num
-            if diff != num:
-                if diff in nums:
-                    return [idx, nums.index(diff)]
+            if diff == num:
+                if len(d[num]) > 1:
+                    return [d[num][0], d[num][1]]
             else:
-                # diff is num
-                l1 = [idx for idx, n in enumerate(nums) if n == num]
-                if len(l1) > 1:
-                    return [l1[0], l1[1]]
+                if len(d[diff]) > 0:
+                    return [idx, d[diff][0]]
 
         return [-1, -1]
 
